a3409c5407532aaf10c296bfc2eaacee5f94063e
samyak360 <samyak360@gmail.com>
2019-02-28 17:17:26 +0530

    added photo, parameters in server and client

--- sih web/temp.py
+++ sih web/temp.py
@@ -4,13 +4,28 @@ from  sklearn  import  tree
  
 # creating dataset for aluminium/plastic/glass/cardboard/paper
 
-# we are calculating moisture/pressure/mouldable/recyclable/transparent
-#/flexible/tearable/temperature/corrosion/lightweight/heatsealable/airtight
-#/flammable/photosensitivity/rxnwitho2/costpercm2/toxic
-#puncture_resistance/elasticity/weight 
+#airtight
+#recyclable
+#toxic
+#moisture_resistance
+#flexible
+#aandb_resist
+#transparent
+#photosensitivity_resistance
+#flammable
+#resist_solvents
+#thickness
+#tensile_strength
+#temperature
+#weightpercm2
+#puncture_resistance
+#costpercm2
 
-features=[[1,10.2,0,1,1,0,0,200,0,0,1,1,0,1,1,15,0,50,0,20.4],[1,30.2,0,1,1,0,0,300,0,0,1,1,0,1,1,25,0,75,0,30.4],[1,45.2,0,1,1,0,0,300,0,0,1,1,0,1,1,25,0,90,0,50.4],[1,4.2,0,1,0,1,0,150,1,0,0,0,0,0,0,0.15,1,8,0,3.4],[1,10.2,0,1,0,0,0,300,1,0,0,1,0,0,0,0.25,1,16,0,6.8],[1,20.2,0,1,0,0,0,300,1,0,0,1,0,0,0,0.40,1,25,0,9.4],[]]
-label=["singlelayerglass","doublelayerglass","triplelayerglass","singlelayeraluminium","doublelayeraluminium","triplelayeraluminium",""]
+
+features=[[1,1,0,1,0,1,1,1,0,1,0.102,10.2,200,0.6,0.21,0.08],[1,1,0,1,0,1,1,1,0,1,0.318,30.2,300,1.4,0.42,0.12],[1,1,0,1,0,1,1,1,0,1,0.417,65.2,300,1.9,0.72,0.21],[0,1,0,1,1,0,0,1,0,1,0.08,4.2,150,0.02,0.13,0.09],[1,1,0,1,1,1,0,1,0,1,0.18,11.2,180,0.08,0.36,0.25],[1,1,0,1,0,1,0,1,0,1,0.27,33.4,180,0.12,0.55,0.40],[0,1,0,0,1,0,0,1,1,0,0.09,4.2,90,0.03,0.14,0.04],[0,1,0,0,1,0,0,1,1,0,0.15,6.9,110,0.09,0.17,0.10],[0,1,0,0,0,0,0,1,1,0,0.20,9.4,130,0.15,0.20,0.17],[0,1,0,0,0,0,0,1,1,0,0.20,9.4,130,0.15,0.35,15.1,130,0.23,0.24,0.40],[1,1,0,1,1,0,0,1,1,1,0.10,8.1,150,0.11,0.16,0.28],[0,1,0,0,1,0,1,0,1,1,0.01,1.1,90,0.001,0.65,0.005],[1,1,0,1,0,1,1,0,0,1,0.82,30.2,220,0.09,0.73,0.43],[1,1,0,1,1,1,1,0,0,1,0.22,18.3,170,0.04,0.49,0.17],[1,1,0,1,0,0,0,1,0,1,0.82,8.3,120,0.11,0.18,0.09],[1,1,0,1,0,0,0,1,0,1,0.92,38.3,320,0.36,3.78,0.60]]
+
+label=["singlelayerglass","doublelayerglass","triplelayerglass","singlelayeraluminium","doublelayeraluminium","triplelayeraluminium","single_faced"
+,"double_faced","single_wall","double_wall","coated","polyethylene","hdpe","LDPE","polystyrene","pvc"]
 
 # now calling  decision tree algo
 algo=tree.DecisionTreeClassifier()
@@ -18,5 +33,5 @@ algo=tree.DecisionTreeClassifier()
 trained=algo.fit(features,label)
 
 # now prediction 
-output=trained.predict([[0,42,0,1,1,0,0,400,0,1,1,1,0,1,0,20,0,14,0,12]])
+output=trained.predict([[]])
 print(output)
