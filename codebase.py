5167024e5468abbecd9a49228413ac2d9d980836
soumil <soumilk.k@gmail.com>
2019-03-02 10:20:36 +0530

    updated packaging.py and codebase.py

--- codebase.py
+++ codebase.py
@@ -15,246 +15,192 @@ firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
 users=db.get()
-
 #print(users.val())
 
+#for user in users.each():
+#    print(user.val())
+
+d={}
 for user in users.each():
-    print(user.key())
-    print(user.val())
+    d[user.key()]=user.val()
 
-
+print(d)
 
 class glass:
-       airtight=1
-       recyclable=1
-       toxic=0
-       moisture_resistance=1
-       flexible=0
-       aandb_resist=1
-       transparent=1
-       photosensitivity_resistance=1
-       flammable=0
-       resist_solvents=1
-       def single_layer(self):
-              thickness=0.102 #cm
-              tensile_strength=10.2 #newton/m^2
-              temperature=200 #degree celcius
-              weightpercm2=0.6    #gm
-              puncture_resistance=0.21 #pascle
-              costpercm2=0.08      #in Rs
+    airtight=1
+    moisture_resistance=1
+    transparent=1
+    photosensitivity_resistance=1
+    flammable=0
+    resist_solvents=1
+    def single_layer(self):
+        thickness=0.102 #cm
+        tensile_strength=10.2 #newton/m^2
+        temperature=200 #degree celcius
+        weightpercm2=0.6    #gm
+        puncture_resistance=0.21 #pascle
+        costpercm2=0.08      #in Rs
 
-       def double_layer(self):
-              thickness=0.318
-              tensile_strength=30.2
-              temperature=300
-              weightpercm2=1.4
-              puncture_resistance=0.42
-              costpercm2=0.12
+    def double_layer(self):
+        thickness=0.318
+        tensile_strength=30.2
+        temperature=300
+        weightpercm2=1.4
+        puncture_resistance=0.42
+        costpercm2=0.12
 
-       def triple_layer(self):
-              thickness=0.417
-              tensile_strength=65.2
-              temperature=300
-              weightpercm2=1.9
-              puncture_resistance=0.72
-              costpercm2=0.21
-
+    def triple_layer(self):
+        thickness=0.417
+        tensile_strength=65.2
+        temperature=300
+        weightpercm2=1.9
+        puncture_resistance=0.72
+        costpercm2=0.21
 
 class aluminium():
-       recyclable=1
-       toxic=0
-       moisture_resistance=1
-       photosensitivity_resistance=1
-       resist_solvents=1
-       flammable=0
-       transparent=0
-       def single_layer(self):
-              airtight=0
-              thickness=0.08 # cm
-              tensile_strength=4.2   #in netwon per m^2
-              temperature=150 #celcius
-              weightpercm2=0.02 #gm
-              puncture_resistance=0.13 #pascle
-              costpercm2=0.09  # in rs
-	      aandb_resist=0
-              flexible=1
+    moisture_resistance=1
+    photosensitivity_resistance=1
+    resist_solvents=1
+    flammable=0
+    def single_layer(self):
+        airtight=0
+        thickness=0.08 # cm
+        tensile_strength=4.2   #in netwon per m^2
+        temperature=150 #celcius
+        weightpercm2=0.02 #gm
+        puncture_resistance=0.13 #pascle
+        costpercm2=0.09  # in rs
 
-       def double_layer(self):
-              airtight=1
-              thickness=0.18
-              tensile_strength=11.2
-              temperature=180
-              weightpercm2=0.08
-              puncture_resistance=0.36
-              costpercm2=0.25
-              flexible=1
-	      aandb_resist=1
-       def triple_layer(self):
-              airtight=1
-              thickness=0.27
-              tensile_strength=33.4
-              temperature=180
-              weightpercm2=0.12
-              puncture_resistance=0.55
-              costpercm2=0.40
-              flexible=0
-              aandb_resist=1
+    def double_layer(self):
+        airtight=1
+        thickness=0.18
+        tensile_strength=11.2
+        temperature=180
+        weightpercm2=0.08
+        puncture_resistance=0.36
+        costpercm2=0.25
+
+    def triple_layer(self):
+        airtight=1
+        thickness=0.27
+        tensile_strength=33.4
+        temperature=180
+        weightpercm2=0.12
+        puncture_resistance=0.55
+        costpercm2=0.40
 
 class cardboard():
-       airtight=0
-       aandb_resist=0
-       recyclable=1
-       toxic=0
-       moisture_resistance=0
-       photosensitivity_resistance=1
-       transparent=0
-       flamable=1
-       resist_solvents=0
-       def single_faced(self):
-              thickness=0.09
-              tensile_strength=4.2
-              temperature=90
-              weightpercm2=0.03
-              puncture_resistance=0.14
-              costpercm2=0.04
-              flexible=1
+    airtight=0
+    toxic=0
+    moisture_resistance=0
+    photosensitivity_resistance=1
+    flamable=1
+    resist_solvents=0
 
-       def double_faced(self):
-              thickness=0.15
-              tensile_strength=6.9
-              temperature=110
-              weightpercm2=0.09
-              puncture_resistance=0.17
-              costpercm2=0.10
-              flexible=1
+    def single_wall(self):
+        thickness=0.20
+        tensile_strength=9.4
+        temperature=130
+        weightpercm2=0.15
+        puncture_resistance=0.20
+        costpercm2=0.17
 
-       def single_wall(self):
-              thickness=0.20
-              tensile_strength=9.4
-              temperature=130
-              weightpercm2=0.15
-              puncture_resistance=0.20
-              costpercm2=0.17
-              flexible=0
+    def double_wall(self):
+        thickness=0.35
+        tensile_strength=15.1
+        temperature=130
+        weightpercm2=0.23
+        puncture_resistance=0.24
+        costpercm2=0.40
 
-       def double_wall(self):
-              thickness=0.35
-              tensile_strength=15.1
-              temperature=130
-              weightpercm2=0.23
-              puncture_resistance=0.24
-              costpercm2=0.40
-              flexible=0
-
-       def coated():
-              thickness=0.10
-              tensile_strength=8.1
-              temperature=150
-              weightpercm2=0.11
-              puncture_resistance=0.16
-              costpercm2=0.28
-              resist_solvents=1
-              flexible=1
-              airtight=1
-              moisture_resistance=1
+    def coated():
+        thickness=0.10
+        tensile_strength=8.1
+        temperature=150
+        weightpercm2=0.11
+        puncture_resistance=0.16
+        costpercm2=0.28
+        airtight=1
+        moisture_resistance=1
 
 class plastic:
+    def polyethylene():
+        airtight=0
+        moisture_resistance=0
+        photosensitivity_resistance=0
+        tensile_strength=1.1
+        puncture_resistance=0.65
+        temperature=90
+        thickness=0.01
+        weightpercm2=0.001
+        costpercm2=0.005
+        resist_solvents=1
+        flamable=1
 
-       def polyethylene():
-              airtight=0
-              recyclable=1
-              toxic=0
-              moisture_resistance=0
-              flexible=1
-              aandb_resist=0
-              photosensitivity_resistance=0
-              tensile_strength=1.1
-              puncture_resistance=0.65
-              temperature=90
-              flamable=1
-              thickness=0.01
-              weightpercm2=0.001
-              costpercm2=0.005
-              resist_solvents=1
-              transparent=1
+    def hdpe():
+        airtight=1
+        moisture_resistance=1
+        photosensitivity_resistance=0
+        tensile_strength=30.3
+        puncture_resistance=0.73
+        temperature=220
+        flamable=0
+        thickness=0.82
+        weightpercm2=0.09
+        costpercm2=0.43
+        resist_solvents=1
 
-       def hdpe():# high density polyethylene
-              airtight=1
-              recyclable=1
-              toxic=0
-              moisture_resistance=1
-              flexible=0
-              aandb_resist=1
-              photosensitivity_resistance=0
-              tensile_strength=30.3
-              puncture_resistance=0.73
-              temperature=220
-              flamable=0
-              thickness=0.82
-              weightpercm2=0.09
-              costpercm2=0.43
-              transparent=1
-              resist_solvents=1
+    def LDPE():
+        airtight=1
+        moisture_resistance=1
+        photosensitivity_resistance=0
+        tensile_strength=18.3
+        puncture_resistance=0.49
+        temperature=170
+        flamable=0
+        thickness=0.22
+        weightpercm2=0.04
+        costpercm2=0.17
+        resist_solvents=1
 
-       def LDPE():
-              airtight=1
-              recyclable=1
-              toxic=0
-              moisture_resistance=1
-              flexible=1
-              aandb_resist=1
-              photosensitivity_resistance=0
-              tensile_strength=18.3
-              puncture_resistance=0.49
-              temperature=170
-              flamable=0
-              thickness=0.22
-              weightpercm2=0.04
-              costpercm2=0.17
-              resist_solvents=1
-              transparent=1
+    def polystyrene():
+        airtight=1
+        moisture_resistance=1
+        photosensitivity_resistance=1
+        tensile_strength=8.3
+        puncture_resistance=0.18
+        temperature=120
+        flamable=0
+        thickness=0.82
+        weightpercm2=0.11
+        costpercm2=0.09
+        resist_solvents=1
 
-       '''def pet:# polyethylene terephthalate
-              transparent
-              oxygen, water,moisture gas barrier
-              high pressure and shatter resistance
-              resist solvents
-       '''
-       def polystyrene():
-              airtight=1
-              recyclable=1
-              toxic=0
-              moisture_resistance=1
-              flexible=0
-              aandb_resist=0
-              photosensitivity_resistance=1
-              tensile_strength=8.3
-              puncture_resistance=0.18
-              temperature=120
-              flamable=0
-              thickness=0.82
-              weightpercm2=0.11
-              costpercm2=0.09
-              resist_solvents=1
-              transparent=0
-
-       def pvc():#Polyvinyl Chloride
-              airtight=1
-              recyclable=1
-              toxic=0
-              moisture_resistance=1
-              flexible=0
-              aandb_resist=0
-              photosensitivity_resistance=1
-              tensile_strength=38.3
-              puncture_resistance=3.78
-              temperature=320
-              flamable=0
-              thickness=0.92
-              weightpercm2=0.36
-              costpercm2=0.60
-              resist_solvents=1
-	          transparent=0
-
+    def pvc():
+        airtight=1
+        moisture_resistance=1
+        photosensitivity_resistance=1
+        tensile_strength=38.3
+        puncture_resistance=3.78
+        temperature=320
+        flamable=0
+        thickness=0.92
+        weightpercm2=0.36
+        costpercm2=0.60
+        resist_solvents=1
 
 # if element is brittle then consider bubble wrap
+print("helloo world")
+# starting with the cardboard
+count=0
+count_list=[]
+c1=cardboard()
+c1.single_wall()
+if d["pressure"]<c1.tensile_strength:
+    count+=1
+if d["moisture"]==c1.moisture_resistance:
+    count+=1
+if (d["odour"]==1 or d["o2"]==1) and c1.airtight==1:
+    count+=1
+if d["temperature"]<c1.temperature:
+    count+=1
--- packing.py
+++ packing.py
@@ -1,80 +1,113 @@
 import math
+import pyrebase
+
+config = {
+  "apiKey": "AIzaSyByA7celafHSloxdOLA7_s-D097Ld10Jus",
+  "authDomain": "firepack-66e50.firebaseapp.com",
+  "databaseURL": "https://firepack-66e50.firebaseio.com",
+  "projectId": "firepack-66e50",
+  "storageBucket": "firepack-66e50.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+
+db = firebase.database()
+
+users=db.get()
+d={}
+for user in users.each():
+    d[user.key()]=user.val()
+print(d)
+
+obj=""
+rad=""
+for user in users.each():
+    if user.key()=="name":
+        obj=user.val()
+    if user.key()=="radius":
+        rad=user.val()
 
 launch_packet_weight=50
 weight_of_object=5
-       
+
 class biscuit:
-       def __init__(self):
-              print("this is the packing procedure for biscuits")
-
-       def packing_shape_cubodial(self,area,length,width,height):
-              pieces=launch_packet_weight/weight_of_object
-              # height is the thickness of biscuit here
-              l=[]
-              cost_to_pack_single=area*#costpercm2 of material + #a small quantity
-              factor=1
-              height_of_packet=width
-              len_of_packet=height*pieces
-              # this is the area of the package when we are packaging
-              # the object in the shape similar to the shape of object
-              package_area=2(len_of_packet*width+len_of_packet*height_of_packet+height_of_packet*width)
-              l.append([1,package_area])
-              while factor<pieces:
-                     factor+=1
-                     height_of_packet=width*factor
-                     len_of_packet=(height*pieces)/factor
-                     package_area=2(len_of_packet*width+len_of_packet*height_of_packet+height_of_packet*width)
-                     print(package_area)
-                     l.append([factor,package_area])
+    def __init__(self):
+        print("this is the packing procedure for biscuits")
+    def packing_shape_cubodial(self,area,length,width,height):
+        pieces=launch_packet_weight/weight_of_object
+        # height is the thickness of biscuit here
+        l=[]
+        cost_to_pack_single=area*#costpercm2 of material + #a small quantity
+        factor=1
+        height_of_packet=width
+        len_of_packet=height*pieces
+        # this is the area of the package when we are packaging
+        # the object in the shape similar to the shape of object
+        package_area=2(len_of_packet*width+len_of_packet*height_of_packet+height_of_packet*width)
+        l.append([1,package_area])
+        while factor<pieces:
+            factor+=1
+            height_of_packet=width*factor
+            len_of_packet=(height*pieces)/factor
+            package_area=2(len_of_packet*width+len_of_packet*height_of_packet+height_of_packet*width)
+            print(package_area)
+            l.append([factor,package_area])
               # for cost
-              for i in range(len(l)):
-                     l[i].append(l[i][1]*)#costpercm2)
+            for i in range(len(l)):
+                l[i].append(l[i][1]*)#costpercm2
 
-       def packing_shape_cylindrical(self,area,radius,thickness):
-              pieces=launch_packet_weight/weight_of_object
-              l=[]
-              cost_to_pack_single=area*costpercm2 # + a small quantity
-              factor=1
-              length_of_packet=thickness*pieces
-              package_area=math.pi*radius**2*length_of_packet
-              # when the packing is done in the shape of the object
-              l.append([factor,packing_area])
-              while factor<pieces:
-                     factor+=1
-                     # now the packing will come in the shape of the cylinder
-                     width=2*r
-                     height_of_packet=factor*2*r
-                     len_of_packet=(thickness*pieces)/factor
-                     package_area=2(len_of_packet*width+len_of_packet*height_of_packet+height_of_packet*width)
-                     l.append([factor,packing_area])
+    def packing_shape_cylindrical(self,area,radius,thickness):
+        pieces=launch_packet_weight/weight_of_object
+        l=[]
+        cost_to_pack_single=area*costpercm2 # + a small quantity
+        factor=1
+        length_of_packet=thickness*pieces
+        package_area=math.pi*radius**2*length_of_packet
+        # when the packing is done in the shape of the object
+        l.append([factor,packing_area])
+        while factor<pieces:
+            factor+=1
+            # now the packing will come in the shape of the cuboid
+            width=2*r
+            height_of_packet=factor*2*r
+            len_of_packet=(thickness*pieces)/factor
+            package_area=2(len_of_packet*width+len_of_packet*height_of_packet+height_of_packet*width)
+            l.append([factor,packing_area])
 
-              for i in range (len(l)):
-                     l[i].append(l[i][1]*)#costpercm2
-                                 
-       def cuboidal():
-              length=0
-              width=0
-              height=0
-              area=2(length*width+length*height+height*width)
-              volume=length*width*height
-              packing_shape_cubodial(self,area,length,width,height)
-              
-       def cylinderical():
-              radius=0
-              thickness=0
-              area=2*math.pi*r*thickness+2*math.pi*r**2
-              volume=math.pi*r**2*thickness
-              packing_shape_cylindrical(area,radius,thickness)
+            for i in range (len(l)):
+                l[i].append(l[i][1]*)#costpercm2
 
-class flour:
-       print("class for flour")
+    def cuboidal():
+        length=int(d["length"])
+        width=int(d["width"])
+        height=int(d["thickness"])
+        area=2(length*width+length*height+height*width)
+        volume=length*width*height
+        packing_shape_cubodial(self,area,length,width,height)
+
+    def cylinderical():
+        radius=int(d["radius"])
+        thickness=int(d["thickness"])
+        area=2*math.pi*r*thickness+2*math.pi*r**2
+        volume=math.pi*r**2*thickness
+        packing_shape_cylindrical(area,radius,thickness)
+
+if obj=="Biscuit":
+    obj1=biscuit()
+    if rad=="":
+        obj1.cuboidal(self)
+    else:
+        obj1.cylinderical(self)
+
+
+class floor:
+       print("class for floor")
 
 # the things which are reactive with oxygen would require the free packing, they are gonna get packed in the packet
 # containing nitrogen so it means that the prodct wont be actually arranged in the particular pattern.
 
 class bingo:
        print("class for bingo")
-       
+
 class Noodles:
        print("class for noodles")
-       
